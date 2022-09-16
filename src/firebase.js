50a50bee8e5e6a8bdf563e51a5b6a4876e5a894b
Roktim Kamal Senapoty <roktimsenapoty2@gmail.com>
2022-09-16 23:46:53 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// require("dotenv").config();
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey:process.env.apiKey,
+  authDomain:process.env.authDomain,
+  projectId:process.env.projectId,
+  storageBucket:process.env.storageBucket,
+  messagingSenderId:process.env.messagingSenderId,
+  appId:process.env.appId,
+  measurementId:process.env.measurementId
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
